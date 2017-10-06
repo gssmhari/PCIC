9b0981b042a8a20aa4ba31c18461bf6da99d20e5
Hariharasudhan <Hariharasudhan.M@flex.com>
2017-10-05 23:18:17 -0700

    [Studio] Add screen "access_denied"

---
+++ client/controllers/app/access_denied.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_access_denied', app_access_denied);
+function app_access_denied($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -51,5 +51,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.access_denied', {
+        views: {
+            app: {
+                controller: 'app_access_denied',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.access_denied');
+                }
+            }
+        }
     });
 });
