968d9dd8ff7e2772ba50bcf256a850b9a528941f
Muthuselvan Murugan <Muthuselvan.Murugan@flex.com>
2017-10-05 22:31:43 -0700

    [Studio] Delete screen "app.notificationscaptivating"

--- client/controllers/app/notificationscaptivating.js
+++
@@ -1,5 +0,0 @@
-angular.module('app').controller('app_notificationscaptivating', app_notificationscaptivating);
-function app_notificationscaptivating($scope, app) {
-    'use strict';
-    app.init($scope);
-}
