47860a4f5c20e3458961a74403ceec793c24e7b1
Muthuselvan Murugan <Muthuselvan.Murugan@flex.com>
2017-10-09 21:03:36 -0700

    [Studio] Modified screen "Login"

--- client/controllers/app/login.js
+++ client/controllers/app/login.js
@@ -2,4 +2,9 @@ angular.module('app').controller('app_login', app_login);
 function app_login($scope, app) {
     'use strict';
     app.init($scope);
+    
+    $scope.surveyBtn = function()
+    {
+    alert('i am here');
+    }
 }
