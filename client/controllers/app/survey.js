edc98ccb2f6c59e62df7716f6772fbdc9756acda
Muthuselvan Murugan <Muthuselvan.Murugan@flex.com>
2017-10-10 02:39:15 -0700

    [Studio] Modified screen "survey"

--- client/controllers/app/survey.js
+++ client/controllers/app/survey.js
@@ -2,4 +2,6 @@ angular.module('app').controller('app_survey', app_survey);
 function app_survey($scope, app) {
     'use strict';
     app.init($scope);
+    
+    $scope.questions=['Question1','Question2','Question3','Question4','Question5'];
 }
